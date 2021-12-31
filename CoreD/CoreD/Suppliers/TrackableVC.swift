2a7f1198058e1137926da9e030f4027d6361ccf8
alanood <alanood@MacBook-Air-alanood.local>
2021-12-31 13:16:48 +0300

    core data task

---
+++ CoreD/CoreD/Suppliers/TrackableVC.swift
@@ -0,0 +1,20 @@
+//
+//  TrackableVC.swift
+//  CoreD
+//
+//  Created by alanood on 26/05/1443 AH.
+//
+
+import UIKit
+
+class TrackableViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
